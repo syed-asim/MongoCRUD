6ea9f22255e37a3e38dc185785f2fa3953d48533
Syed Asim <asim.softwareengineer@hotmail.com>
2021-01-12 03:51:08 +0500

    fg

--- MongoCRUD/Program.cs
+++ MongoCRUD/Program.cs
@@ -14,26 +14,96 @@ namespace MongoCRUD
         static void Main(string[] args)
         {
 
-            var client = new MongoClient("mongodb+srv://syed:qwer1234@cluster0.vwpbm.mongodb.net/mongo_test? retryWrites =true&w=majority");
-            var database = client.GetDatabase("mongo_test");
-            var collection = database.GetCollection<BsonDocument>("sales_data");
 
-            // Data Insert >>>
-            ////var jsonData = ConvertCsvFileToJsonObject(@"c:/10K Sales Records.csv");
-            ////var BSONDoc = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<List<BsonDocument>>(jsonData);
-            ////collection.InsertMany(BSONDoc);
+            IMongoCollection<BsonDocument> collection = GetCollection();
 
-            // Method 1 >>>
+            // >> Data Insert >>>
+            // ImportData(collection);
+
+            // >> Bucket Auto Example
+            //BucketAuto(collection);
+
+            // >> Method 1 
             var group = new BsonDocument(
-                "$group", new BsonDocument {
-                    {"_id", new BsonDocument {{ "Region", "$Region" },/*{ "roomtype", "$room_type" }*/}},
+                 "$group", new BsonDocument {
+                    {"_id", new BsonDocument {{ "Region", "$Region" },{ "Country", "$Country" } }},
                     {"TotalProfit", new BsonDocument("$sum", "$TotalProfit")},
-                    //{"totalBeds", new BsonDocument ("$sum", "$beds")}
-                });
+                    {"TotalCost", new BsonDocument("$sum", "$TotalCost")},
+                 });
 
-             var sort = new BsonDocument("$sort", new BsonDocument { { "TotalProfit", -1 }, /*{ "totalBeds", -1 }*/ });
-            var pipeline = new[] {/* match,project,*/ group,sort };
-            var result = collection.Aggregate<BsonDocument>(pipeline).ToList();
+
+
+            var match3 = new BsonDocument
+                {
+                    {
+                        "$match",
+                        new BsonDocument
+                            {
+                                {"Count", new BsonDocument
+                                                   {
+                                                       {
+                                                           "$gte", 2
+                                                       }
+                                                   }}
+                            }
+                    }
+                };
+
+            var match2 = new BsonDocument
+                        {
+                            {
+                                "$match",
+                                new BsonDocument
+                                {
+                                    { "Region", new BsonDocument{
+                                        {  "$in",
+                                        new BsonArray {
+                                            "Sub-Saharan Africa" ,
+                                            "Europe"
+                                        } }
+                                        } }
+                                }
+                            }
+                        };
+
+
+            var sort = new BsonDocument("$sort", new BsonDocument { { "TotalProfit", -1 }, /*{ "totalBeds", -1 }*/ });
+            var pipeline = new[] { match2,/*project,*/ group, sort };
+
+            var db = GetDatabase();
+            var match = new BsonDocument
+                        {
+                            {
+                                "$match",
+                                new BsonDocument
+                                {
+                                    {"Region", "Asia"}
+                                }
+                            }
+                        };
+            var proj = new BsonDocument
+                        {
+                            {
+                                "$project",
+                                new BsonDocument
+                                {
+                                    {"_id", 0},
+                                    {"Region","$_id.Region"},
+                                    {"Country","$_id.Country"},
+                                    {"TotalProfit",1 },
+                                    {"TotalCost",1 }
+                                }
+                            }
+                        };
+            var pipelineForView = new BsonDocument[] { match2,/*project,*/ group, sort, proj };
+            db.CreateView<BsonDocument, BsonDocument>("rgo", "sales_data", pipelineForView);
+            var vw = db.GetCollection<BsonDocument>("rgo");
+            var rs = vw.CountDocuments(new BsonDocument { });
+
+
+
+            var aggregateOptions = new AggregateOptions { AllowDiskUse = true };
+            var result = collection.Aggregate<BsonDocument>(pipeline, aggregateOptions).ToList();
 
 
             // Method 2 >>>
@@ -52,6 +122,36 @@ namespace MongoCRUD
             {
                 Console.WriteLine(example);
             }
+        }
+
+        private static void ImportData(IMongoCollection<BsonDocument> collection)
+        {
+            var jsonData = ConvertCsvFileToJsonObject(@"c:/10K Sales Records.csv");
+            var BSONDoc = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<List<BsonDocument>>(jsonData);
+            collection.InsertMany(BSONDoc);
+        }
+
+        private static IMongoCollection<BsonDocument> GetCollection()
+        {
+            IMongoDatabase database = GetDatabase();
+            var collection = database.GetCollection<BsonDocument>("sales_data");
+            return collection;
+        }
+
+        private static IMongoDatabase GetDatabase()
+        {
+            var client = new MongoClient("mongodb+srv://syed:qwer1234@cluster0.vwpbm.mongodb.net/mongo_test? retryWrites =true&w=majority");
+            var database = client.GetDatabase("mongo_test");
+            return database;
+        }
+
+        private static void BucketAuto(IMongoCollection<BsonDocument> collection)
+        {
+            var groupBy = (AggregateExpressionDefinition<BsonDocument, BsonValue>)"$Region";
+            var boundaries = new BsonValue[] { 1900, 1920, 1950 };
+            var output = (ProjectionDefinition<BsonDocument, BsonDocument>)"{ Regions : { $push : \"$Region\" }, count : { $sum : 1 } }";
+            var autoBucket = collection.Aggregate().BucketAuto(groupBy, 1);
+            var autoBucketResults = autoBucket.ToList();
         }
 
         public static string ConvertCsvFileToJsonObject(string path)
@@ -113,7 +213,7 @@ namespace MongoCRUD
                 }
                 listObjResult.Add(objResult);
             }
-            Console.WriteLine("Object to serialize count: "+listObjResult.Count);
+            Console.WriteLine("Object to serialize count: " + listObjResult.Count);
             return Newtonsoft.Json.JsonConvert.SerializeObject(listObjResult);
         }
 
